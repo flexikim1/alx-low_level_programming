7f331a2cc5ca5bbe86c646aabefa153c6a601fe7
Hakeem <akinogunlaja@gmail.com>
2023-03-18 13:02:06 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main - print a c program using printf
  *
- * Return: Always 0 (Success)
- */
+ * Return: 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingua puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (o);
 }
