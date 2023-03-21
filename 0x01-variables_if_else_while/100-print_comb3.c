7e52531522fafd9daadc7713ef0494c45f38fef6
Hakeem <akinogunlaja@gmail.com>
2023-03-21 11:47:26 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - main function
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 0 ; j < 10 ; j++)
+		for (j = 1 ; j < 10 ; j++)
 		{
 			if (i < j && i != j)
 			{
