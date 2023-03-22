eaaeda31c062baae34e6c68de1edafe6715438d7
Hakeem <akinogunlaja@gmail.com>
2023-03-22 01:38:35 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-#include<stdio>
+#include <stdio.h>
 
 /**
  * main - main function
@@ -8,7 +8,7 @@
 int main(void)
 {
 	char i;
-	
+
 	for (i = 'a' ; i <= 'z' ; i++)
 		if (i != 'q' && i != 'e')
 			putchar(i);
